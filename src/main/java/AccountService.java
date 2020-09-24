c8fd1fdad6e42bbf046c594e06c434a24c30bb50
Anna.Klyuchuk <nusika_93@mail.ru>
2020-09-24 19:39:22 +0400

    Anna

--- src/main/java/AccountService.java
+++ src/main/java/AccountService.java
@@ -1,9 +1,11 @@
+import java.sql.SQLException;
+
 public interface AccountService {
-    void balance(int accountId) throws UnknownAccountException;
+    void balance(int accountId) throws UnknownAccountException, SQLException;
 
-    void withdraw(int accountId, int amount) throws NotEnoughMoneyException, UnknownAccountException;
+    void withdraw(int accountId, int amount) throws NotEnoughMoneyException, UnknownAccountException, SQLException;
 
-    void deposit(int accountId, int amount) throws NotEnoughMoneyException, UnknownAccountException;
+    void deposit(int accountId, int amount) throws NotEnoughMoneyException, UnknownAccountException, SQLException;
 
-    void transfer(int from, int to, int amount) throws NotEnoughMoneyException, UnknownAccountException;
+    void transfer(int from, int to, int amount) throws NotEnoughMoneyException, UnknownAccountException, SQLException;
 }
--- src/main/java/UnknownAccountException.java
+++ src/main/java/UnknownAccountException.java
@@ -1,3 +1,5 @@
+
+
 public class UnknownAccountException extends RuntimeException {
     public UnknownAccountException() {
         super("Неизвестный аккаунт");
